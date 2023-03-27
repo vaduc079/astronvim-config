c665e8848a9a4f0374356582cf5e0f2143a33ccb
ducva <ducvuanh.79@gmail.com>
2023-03-28 00:39:05 +0700

    Adds surround and ts-rainbow2

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,43 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup {
+        -- Configuration here, or leave empty to use defaults
+      }
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    dependencies = {
+      "HiPhish/nvim-ts-rainbow2",
+      config = function()
+        -- HACK: https://github.com/p00f/nvim-ts-rainbow/issues/112#issuecomment-1310835936
+        vim.api.nvim_create_autocmd({ "BufWritePost", "FocusGained" }, {
+          callback = function()
+            vim.cmd "TSDisable rainbow"
+            vim.cmd "TSEnable rainbow"
+          end,
+        })
+        require("nvim-treesitter.configs").setup {
+          rainbow = {
+            hlgroups = {
+              "TSRainbowYellow",
+              "TSRainbowOrange",
+              "TSRainbowGreen",
+              "TSRainbowViolet",
+              "TSRainbowBlue",
+              "TSRainbowCyan",
+              "TSRainbowRed",
+            },
+          },
+        }
+      end,
+    },
+    opts = { rainbow = { enable = true } },
+  },
 }
