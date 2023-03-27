64a88cdc320ac1b9a8962a137267e0289e081379
ducva <ducvuanh.79@gmail.com>
2023-03-28 00:39:19 +0700

    Adds custom mapping

--- mappings.lua
+++ mappings.lua
@@ -11,7 +11,9 @@ return {
     ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
     ["<leader>bD"] = {
       function()
-        require("astronvim.utils.status").heirline.buffer_picker(function(bufnr) require("astronvim.utils.buffer").close(bufnr) end)
+        require("astronvim.utils.status").heirline.buffer_picker(
+          function(bufnr) require("astronvim.utils.buffer").close(bufnr) end
+        )
       end,
       desc = "Pick to close",
     },
@@ -20,9 +22,27 @@ return {
     ["<leader>b"] = { name = "Buffers" },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+
+    -- center search result
+    ["n"] = { "nzz" },
+    ["N"] = { "Nzz" },
+    -- switch buffer
+    ["<S-h>"] = { ":bprevious<cr>", desc = "Switch to previous buffer" },
+    ["<S-l>"] = { ":bnext<cr>", desc = "Switch to next buffer" },
   },
   t = {
     -- setting a mapping to false will disable it
     -- ["<esc>"] = false,
+  },
+  v = {
+    -- paste over currently selected text without yanking it
+    ["p"] = { '"_dP' },
+    -- better indent
+    ["<"] = { "<gv", desc = "Better unindent" },
+    [">"] = { ">gv", desc = "Better indent" },
+  },
+  x = {
+    ["K"] = { ":move '<-2<cr>gv-gv", desc = "Move line/block up" },
+    ["J"] = { ":move '>+1<cr>gv-gv", desc = "Move line/block down" },
   },
 }
