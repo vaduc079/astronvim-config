d6fbe131fafca2d49849f2eb446d17583fa858c1
ducva <ducvuanh.79@gmail.com>
2023-03-28 00:38:23 +0700

    Adds community plugins for markdown, ts, lua

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,9 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.kanagawa" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.lua" },
 }
