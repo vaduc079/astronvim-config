52d865ec94b5cd397163d3cd7bc6f5744dde7db0
ducva <ducvuanh.79@gmail.com>
2023-04-01 01:31:07 +0700

    Added markdown-preview.nvim

--- plugins/user.lua
+++ plugins/user.lua
@@ -48,4 +48,9 @@ return {
     },
     opts = { rainbow = { enable = true } },
   },
+  {
+    "iamcco/markdown-preview.nvim",
+    build = function() vim.fn["mkdp#util#install"]() end,
+    ft = "markdown",
+  },
 }
