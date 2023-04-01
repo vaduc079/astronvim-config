7e926e95e35fa534a7d54a9a82e3823abc02bf72
ducva <ducvuanh.79@gmail.com>
2023-04-01 17:18:21 +0700

    customize default astronvim plugins config

--- plugins/community.lua
+++ plugins/community.lua
@@ -5,6 +5,13 @@ return {
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
   { import = "astrocommunity.colorscheme.kanagawa" },
+  {
+    "rebelot/kanagawa.nvim",
+    opts = {
+      commentStyle = { italic = false },
+      keywordStyle = { italic = false },
+    },
+  },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.pack.markdown" },
   { import = "astrocommunity.pack.typescript" },
